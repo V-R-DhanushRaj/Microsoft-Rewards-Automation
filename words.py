0ec5853aafe155b3abbae1964ab73e56101dbf09
V-R-DhanushRaj <vrdhanushraj@gmail.com>
2025-07-25 09:22:45 +0530

    More words added. {NO = 77}

--- words.py
+++ words.py
@@ -1,3 +1,12 @@
-WORDS_LIST = ['Python', 'Java', 'C#', 'Programming', 'C', 'Coding Tutorial', 'C++', 'Ruby', 'Go Programming Language',
-              'Top Coding Languages', 'Top Tech News', 'Latest Phone Release', 'LeetCode', 'VS Code', 'IDE',
-              'Top programming tools', 'Linux', 'PC vs CPU', 'discord', 'New iphone leak']
+WORDS_LIST = [
+    'Python', 'Java', 'C#', 'Programming', 'C', 'Coding Tutorial', 'C++', 'Ruby', 'Go Programming Language', 'AI', 'JS',
+    'Top Coding Languages', 'Top Tech News', 'Latest Phone Release', 'LeetCode', 'VS Code', 'IDE', 'Devops', 'OS',
+    'Top programming tools', 'Linux', 'PC vs CPU', 'discord', 'New iphone leak', 'Types of Complexity in CS', 'API',
+    'Data structure and algorithm', 'Time Complexity', 'Space Complexity', 'IoT', 'DSA full form', 'blockchain', 'AWS',
+    'Github Copilot', 'low-level vs high-level lang', 'game dev', 'Azure', 'docker', 'AI vs ML vs Deep learning', 'iOS',
+    'NLP explained', 'Kernel', 'Shell', 'OSI model layers', 'Threads vs processes', 'Deadlock in OS', 'How OS work',
+    'supervised vs unsupervised learning', 'APK', 'AAB', 'Flutter', 'Android', 'HTTP full form', 'DOM manipulation',
+    'CSS Flexbox', 'CSS Grid', 'Cookies', 'local storage', 'Git vs Github', 'REST API vs SOAP', 'Malware', 'Ransomware',
+    'SQL injection', 'SSL vs TLS', 'Encryption explained', 'Calculus for CS', 'Stack', 'queue', 'Compiler', 'Hash map',
+    'Interpreter', 'Compiler', 'Recursion', 'iteration', 'Dynamic typing', 'Static typing', 'OOP explained'
+]
